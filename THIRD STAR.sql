6af70ff81f05dd248e708e45b8998ab6b371d4dc
jatin3110 <52653077+jatin3110@users.noreply.github.com>
2020-07-23 13:57:38 +0530

    Update THIRD STAR.sql

--- THIRD STAR.sql
+++ THIRD STAR.sql
@@ -78,4 +78,38 @@ SELECT name
     FROM employee   
         WHERE 
             salary > 2000 AND months < 10 
-                ORDER BY employee_id;              
+                ORDER BY employee_id;    
+                                                                                 
+# Weather Observation Station 18
+                                                                                 
+ #Consider  and  to be two points on a 2D plane.
+
+# happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+# happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+# happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+# happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+# Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+
+# Input Format
+
+# The STATION table is described as follows:
+
+# where LAT_N is the northern latitude and LONG_W is the western longitude.
+                                                                                 
+select 
+    Round(ABS(MIN(LAT_N) - MAX(LAT_N)) + ABS(MIN(LONG_W) - MAX(LONG_W)),4)
+        FROM STATION;                                                                                 
+
+# Weather Observation Station 19
+# Consider  and  to be two points on a 2D plane where  are the respective minimum and maximum values of Northern Latitude (LAT_N) and  are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+
+# Query the Euclidean Distance between points  and  and format your answer to display  decimal digits.
+
+# Input Format
+
+# The STATION table is described as follows:
+                                             
+
+SELECT
+    ROUND(SQRT(POWER(MAX(LAT_N)-MIN(LAT_N),2)+POWER(MAX(LONG_W)-MIN(LONG_W),2)),4)
+FROM STATION;

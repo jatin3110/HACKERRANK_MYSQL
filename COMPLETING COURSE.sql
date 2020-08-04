307a16176391f39cb059960e7aaee84820f7b05d
jatin3110 <52653077+jatin3110@users.noreply.github.com>
2020-08-04 10:06:24 +0530

    Update COMPLETING COURSE.sql

--- COMPLETING COURSE.sql
+++ COMPLETING COURSE.sql
@@ -1,5 +1,5 @@
 # Revising Aggregations - The Count Function
-# Query a count of the number of cities in CITY having a Population larger than .
+# Query a count of the number of cities in CITY having a Population larger than.
 
 SELECT     
     COUNT(NAME)
